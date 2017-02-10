882fa24fbf3b0b288469630087ef17cdbaddd69b
Peng Zhong <peng@nylira.com>
2017-02-10 12:41:12 +0800

    add page test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
+  PageTest: require('./src/PageTest.vue'),
   Reset: require('./src/PageUserReset.vue'),
   Settings: require('./src/PageUserSettings.vue'),
   SignIn: require('./src/PageUserSignIn.vue'),

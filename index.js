b005d67207608dc94bc17a9b0c3f291707a9e177
Peng Zhong <peng@nylira.com>
2017-02-10 12:40:15 +0800

    fix links

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  Reset: require('./src/PageUserSignUp.vue'),
-  Settings: require('./src/PageUserSignUp.vue'),
+  Reset: require('./src/PageUserReset.vue'),
+  Settings: require('./src/PageUserSettings.vue'),
   SignIn: require('./src/PageUserSignIn.vue'),
   SignUp: require('./src/PageUserSignUp.vue')
 }
